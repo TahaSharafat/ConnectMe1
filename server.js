94b919d26ce00a704a5923ec3086465bec57e2a7
TahaSharafat <31868641+TahaSharafat@users.noreply.github.com>
2017-12-19 14:04:09 -0500

    testing again

--- server.js
+++ server.js
@@ -2,43 +2,18 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app); //creates server
 var io = require('socket.io').listen(server);
-
 users = {};
 connections = [];
 app.use(express.static('public'));
 
-var db_config = {
-    host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'b2688ca46574e6',
-    password: '5ac14581',
-    database: 'heroku_48febb90a6d362c'
-};
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-05.cleardb.net',
+  user     : 'b6d6c6e874',
+  password : '5ac14581',
+  database : 'heroku_48febb90a6d362c'
+});
 
-var connection;
-
-function handleDisconnect() {
-    console.log('1. connecting to db:');
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-													// the old one cannot be reused.
-
-    connection.connect(function(err) {              	// The server is either down
-        if (err) {                                     // or restarting (takes a while sometimes).
-            console.log('2. error when connecting to db:', err);
-            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
-        }                                     	// to avoid a hot loop, and to allow our node script to
-    });                                     	// process asynchronous requests in the meantime.
-    											// If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-        console.log('3. db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
-            handleDisconnect();                      	// lost due to either server restart, or a
-        } else {                                      	// connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
-
-handleDisconnect();
+connection.connect();
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
@@ -46,7 +21,7 @@ app.get('/',function(req,res){
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
 
 io.sockets.on('connection', function(socket){
@@ -117,6 +92,7 @@ io.sockets.on('connection', function(socket){
 		io.sockets.emit('get users', Object.keys(users));
 	}
 });
+
 
 // 404 handler
 app.use(function(req,res) {
